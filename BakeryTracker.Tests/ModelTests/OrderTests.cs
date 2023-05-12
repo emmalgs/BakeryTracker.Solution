5e46f7dd2bdabbee07dab015132dcaa0f88603bc
Emma Gerigscott <elgerig@gmail.com>
2023-05-12 09:41:35 -0700

    test amount, price, and date fields of instance of order + tests passing

--- BakeryTracker.Tests/ModelTests/OrderTests.cs
+++ BakeryTracker.Tests/ModelTests/OrderTests.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using BakeryTracker.Models;
 using System;
+using System.Globalization;
 
 namespace BakeryTracker.Tests
 {
@@ -32,5 +33,28 @@ namespace BakeryTracker.Tests
       Assert.AreEqual(expected, newOrder.Title);
     }
 
+    [TestMethod]
+    public void Order_HasAmount_Int()
+    {
+      Order newOrder = new Order("croissant", 5);
+      int expected = 5;
+      Assert.AreEqual(expected, newOrder.Amount);
+    }
+
+    [TestMethod]
+    public void Order_HasPrice_Float()
+    {
+      Order newOrder = new Order("croissant", 5);
+      float expected = 1.50F;
+      Assert.AreEqual(expected, newOrder.Price);
+    }
+
+    [TestMethod]
+    public void Order_HasDate_String()
+    {
+      Order newOrder = new Order("croissant", 5);
+      string expected = "2023-05-12";
+      Assert.AreEqual(expected, newOrder.Date.ToString("yyyy-MM-dd"));
+    }
   }
 }
